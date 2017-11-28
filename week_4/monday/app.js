99b88450a642d8b15a262d5050c1ac47c7987c3e
Frederick Amstutz <famstutz@Fredericks-MacBook-Pro.local>
2017-11-27 20:33:54 -0500

    Practicing in class

---
+++ week_4/monday/app.js
@@ -0,0 +1,7 @@
+var myName = 'Fred Amstutz';
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.slice(5,12));
+console.log(myName.replace('Amstutz', 'Tacos'));
+
