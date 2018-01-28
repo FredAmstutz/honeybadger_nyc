85d154f03d6368be07e0a21f356b81510b88f7c1
FredAmstutz <fred4892@gmail.com>
2018-01-28 13:59:32 -0500

    End of class 1/28

--- week_12/wed/spotify-app/app.js
+++ week_12/wed/spotify-app/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
 const request = require('request-promise');
+const exphbs  = require('express-handlebars');
+const favicon = require('serve-favicon');
+const path = require('path');
 const { clientId, clientSecret }= require('./api_key');
 const app = express();
 
 app.use(logger('dev'));
+app.engine('handlebars', exphbs({defaultLayout: 'index'}));
+app.set('view engine', 'handlebars');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -53,6 +59,23 @@ function getTopTracks(access_token, id) {
     })
 }
 
+function normalizeTrackData(track) {
+    const {
+        album: {
+            name: albumName
+        },
+        name: trackName,
+        artists: [{
+            name: artistName
+        }]
+    } = track;
+    return {
+        albumName,
+        trackName,
+        artistName
+    }
+}
+
 app.get('/:artist', function(req, res) {
     const artist = req.params.artist;
     let _access_token;
@@ -69,7 +92,14 @@ app.get('/:artist', function(req, res) {
             return getTopTracks(_access_token, id);
         })
         .then(function(topTracks) {
-            res.send(topTracks);
+            const normalizedTracks = topTracks.tracks.map(function(track) {
+                return normalizeTrackData(track);
+            })
+
+            const result = {
+                tracks: normalizedTracks
+            }
+            res.render('artists', result);
         })
 });
 
