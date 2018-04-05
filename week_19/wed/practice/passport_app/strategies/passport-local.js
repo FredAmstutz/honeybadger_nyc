8485e74a77894b3d9f708428c55a83b22f7d868d
FredAmstutz <fred4892@gmail.com>
2018-04-04 21:19:01 -0400

    End of class Wedneday 4/4

---
+++ week_19/wed/practice/passport_app/strategies/passport-local.js
@@ -0,0 +1,34 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/index').user;
+
+function processSignupCallback(req, email, password, done) {
+    User.findOne({
+        where: {
+            'email': email
+        },
+        attributers: ['id']
+    })
+    .then(function(user) {
+        if (user) {
+            return done(null, false)
+        } else {
+            User.create(req.body)
+            .then(function(createdRecord) {
+                createdRecord.password = undefined;
+                return done(null, createdRecord);
+            });
+        };
+    });
+};
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.use('local-signup', new LocalStrategy ({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, processSignupCallback));
+};
