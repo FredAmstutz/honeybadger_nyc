737bfb409ec1e017be5c444beb58ad31a889d724
FredAmstutz <fred4892@gmail.com>
2018-04-12 20:36:44 -0400

    Preparing to submit cookie confenctionary

---
+++ homework/cookie-confectionary/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CookieConfectionary from './components/cookie-confectionary';
+import './index.css';
+
+ReactDOM.render(<CookieConfectionary />, document.getElementById('root'));
